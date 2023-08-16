5810d91fa2841e93044045a9856bbd81907c64a4
Sumitgiri15434 <96234273+SumitKumargiri@users.noreply.github.com>
2023-08-16 19:35:21 +0530

    Add files via upload

---
+++ Source/librarian/user/contactus.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Digital Library Management System</title>
+	<link rel="icon" type="image/png" href="dist/img/mm.png">
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="dist/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="dist/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="dist/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="dist/css/animate.css">
+	<link rel="stylesheet" href="dist/css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Exo+2:400,500,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-3">
+					<div class="logo">
+						<img src="dist/img/1.png" alt="logo">
+					</div>
+				</div>
+				<div class="col-9">
+					<div class="header-right">
+						<ul>
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin"></i></a></li>
+							<li><a href="index.php">Home</a></li>
+						</ul>		
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contactinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="left">
+						<h3>Send Us a Message</h3>
+						<form action="https://formspree.io/f/xbjvgavw" method="POST">
+							<input type="text" placeholder="Your Name" name="name">
+							<input type="email" placeholder="Your E-mail" name="email">
+							<input type="digit" placeholder="Phone Number" name="phonenumber">
+							<textarea placeholder="Your Message" name="message" id="" cols="60" rows="5"></textarea>
+							<input type="submit" value="SEND A MESSAGE">
+						</form>
+
+						<!-- <form action="https://formspree.io/f/xbjvgavw" method="POST">
+						    <label>Your email:<input type="email" name="email">
+						</label>
+						<label>
+							Your message:
+							<textarea name="message"></textarea>
+						</label>your other form fields go here 
+						<button type="submit">Send</button>
+						</form> -->
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="right">
+						<h3>Contact Info</h3>
+						<p>Our Management Training Programmes can be tailored as in-house programmes to address specific issues within your own company.We figure that most people who end up managing other people never really set off to do that in the first place.</p>
+
+						<div class="box">
+							<p><i class="fas fa-home"></i><span> R/A 14 #48 Bettiah 2nd phase</span></p>
+							<p><i class="far fa-envelope"></i><span> sumitgiri15434@gmail.com</span></p>
+							<p><i class="fas fa-phone"></i><span> 9128205312</span></p>
+							<p>9102857113</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="map-responsive mapw">
+		<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d459.6851149337604!2d89.543789542394!3d22.821690263603124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff9aaf52672c3d%3A0x7457988431037161!2sSonadanga+R%2FA%2C+(2nd+Phase).%2C+Khulna!5e0!3m2!1sen!2sbd!4v1523867957528" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14212.630306061328!2d84.4497833549469!3d27.056773624982114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399362989052919d%3A0xc847f1663cba5832!2sBijbania%2C%20Bihar%20845307!5e0!3m2!1sen!2sin!4v1691914796928!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen></iframe>
+	</div>
+    <div class="footer text-center">
+        <p style="margin: 20px; font-weight: bold;"><span>copyright &copy; 2023 all rights reserved</span></p>
+    </div>
+
+	<script src="dist/js/jquery-2.2.4.min.js"></script>
+	<script src="dist/js/bootstrap.min.js"></script>
+	<script src="dist/js/owl.carousel.min.js"></script>
+	<script src="dist/js/owl.animate.js"></script>
+	<script src="dist/js/superfish.js"></script>
+	<script src="dist/js/waypoints.min.js"></script>
+	<script src="dist/js/jquery.counterup.min.js"></script>
+	<script src="dist/js/custom.js"></script>
+</body>
+</html>
